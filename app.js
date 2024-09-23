fe583db72dfb4219f45645c46e32deec1f7d0455
Ramona Ciceu <77670928+Ramona-Ciceu@users.noreply.github.com>
2024-09-23 10:27:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Your WeatherAPI.com API key
-const apiKey = '40eed157c5f0493d9ff133245240609'; 
+const apiKey = '9a75a6cb38999af4e3306038f4a726e9'; 
 
 // Get references to HTML elements
 const searchButton = document.getElementById('search-button');
